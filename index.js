a5438951826b2ed8089820ce7548d8aaa1e8d528
Saad Malik <saad_malik200@hotmail.com>
2022-08-24 14:45:42 +0200

    ex2

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    // Complete the code!
+
+    // console.log(`${this.firstname} ${this.lastname}`);
+  }
+
+  firstName() {
+    console.log(`${this.firstname}`);
+  }
+  lastName() {
+    console.log(`${this.lastname}`);
+  }
+
+  createFullName() {
+    console.log(`${this.firstname} ${this.lastname}`);
+  }
+
+  createEmail() {
+    console.log(
+      `${this.firstname.toLowerCase()}.${this.lastname.toLowerCase()}@company.com`
+    );
+  }
 }
+
+const emp1 = new Employee("Saad", "Malik");
+
+emp1.createFullName();
+emp1.createEmail();
+
+const emp2 = new Employee("John", "Snow");
+emp2.createFullName();
+emp2.createEmail();
+emp2.firstName();
+emp1.lastName();
+// const email = new Employee("Saad", "Malik");
+
+// email.createEmail();
